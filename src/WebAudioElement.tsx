27c4ba9792f977f8483695e44e00f7ba83a17fdf
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2022-01-18 23:14:32 +0530

    web audio element that will support volume etc.

---
+++ src/WebAudioElement.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export interface WebAudioElementProps {}
+
+const WebAudioElement = (props: WebAudioElementProps) => {
+  return <div></div>;
+};
+
+export default WebAudioElement;
