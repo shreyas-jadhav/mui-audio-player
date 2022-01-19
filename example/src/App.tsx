1eb2256d693742e7ae52088a43c1f7da3a9beafb
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2022-01-19 14:42:46 +0530

    added waveform and regular player

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,25 +1,205 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { createTheme, ThemeProvider, useTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import MuiAudioPlayer from "mui-audio-player";
+import IconButton from "@mui/material/IconButton";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import Card from "@mui/material/Card";
+import CodeIcon from "@mui/icons-material/Code";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
 
+const mp3Src = require("./test.mp3");
 function App() {
+  const [mode, setMode] = useState<"light" | "dark">("dark");
+  const toogleMode = () =>
+    setMode((prev) => (prev === "light" ? "dark" : "light"));
+
+  const theme = useTheme();
+  const variants = [
+    {
+      name: "Inline Regular",
+      content: (
+        <MuiAudioPlayer
+          id="inline-timeline"
+          display="timeline"
+          containerWidth={300}
+          inline
+          src={mp3Src}
+        />
+      ),
+      code: `<MuiAudioPlayer
+          id="inline-timeline"
+          display="timeline"
+          containerWidth={300}
+          inline
+          src={mp3Src}
+        />`,
+    },
+    {
+      name: "Inline Waveform",
+      content: (
+        <MuiAudioPlayer id="inline" containerWidth={350} inline src={mp3Src} />
+      ),
+      code: `<MuiAudioPlayer id="inline" inline src={mp3Src} />,`,
+    },
+    {
+      name: "Without Timestamps",
+      content: (
+        <MuiAudioPlayer
+          id="without-timestamp"
+          showTimestamps={false}
+          containerWidth={350}
+          inline
+          src={mp3Src}
+        />
+      ),
+      code: `
+      <MuiAudioPlayer
+          id="without-timestamp"
+          showTimestamps={false}
+          containerWidth={350}
+          inline
+          src={mp3Src}
+        />
+      `,
+    },
+    {
+      name: "Inline false, Paperize false",
+      content: (
+        <MuiAudioPlayer
+          id="regular"
+          containerWidth={300}
+          paperize={false}
+          src={mp3Src}
+        />
+      ),
+      code: `
+      <MuiAudioPlayer
+          id="regular"
+          containerWidth={300}
+          paperize={false}
+          src={mp3Src}
+        />
+      `,
+    },
+    {
+      name: "Customization",
+      content: (
+        <MuiAudioPlayer
+          id="custom"
+          src={mp3Src}
+          inline
+          display="waveform"
+          waveColor="green"
+          containerWidth={400}
+          playPauseIconButtonProps={{
+            sx: {
+              backgroundColor: theme.palette.primary.main,
+              color: theme.palette.primary.contrastText,
+              "&:hover": {
+                backgroundColor: theme.palette.primary.dark,
+              },
+            },
+          }}
+        />
+      ),
+      code: `
+      
+        <MuiAudioPlayer
+          id="custom"
+          src={mp3Src}
+          inline
+          display="waveform"
+          waveColor="green"
+          containerWidth={400}
+          playPauseIconButtonProps={{
+            sx: {
+              backgroundColor: theme.palette.primary.main,
+              color: theme.palette.primary.contrastText,
+              "&:hover": {
+                backgroundColor: theme.palette.primary.dark,
+              },
+            },
+          }}
+        />
+      
+      `,
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider
+      theme={createTheme({
+        palette: {
+          mode,
+        },
+      })}
+    >
+      <CssBaseline />
+
+      <Container
+        component="main"
+        sx={{
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+        }}
+      >
+        <Grid container spacing={2} justifyContent="center" alignItems="center">
+          <Grid
+            item
+            spacing={2}
+            justifyContent="center"
+            alignItems="center"
+            container
+          >
+            <Grid item>
+              <Typography textAlign="center" gutterBottom variant="h4">
+                MUI Audio Player Demo
+              </Typography>
+            </Grid>
+            <Grid item justifyContent="center" alignItems="center">
+              <IconButton onClick={toogleMode}>
+                {mode === "light" ? <LightModeIcon /> : <DarkModeIcon />}
+              </IconButton>
+            </Grid>
+          </Grid>
+          {variants.map((v) => (
+            <Grid item key={v.name}>
+              <Card variant="elevation" elevation={0} sx={{ p: 2 }}>
+                <Accordion variant="elevation" elevation={0}>
+                  <AccordionSummary expandIcon={<CodeIcon />}>
+                    <Box mx={2}>
+                      <Box>
+                        <Typography
+                          gutterBottom
+                          variant="h6"
+                          textAlign="center"
+                        >
+                          {v.name}
+                        </Typography>
+                      </Box>
+                    </Box>
+                  </AccordionSummary>
+                  <AccordionDetails sx={{ width: 300 }}>
+                    <code>{v.code}</code>
+                  </AccordionDetails>
+                </Accordion>
+                <Box>{v.content}</Box>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </ThemeProvider>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,280 @@
-import React from "react";
-import MuiHtmlAudioElement, {
-  MuiHtmlAudioElementProps,
-} from "./MuiHtmlAudioElement";
-import WebAudioElement, { WebAudioElementProps } from "./WebAudioElement";
+import React, {
+  useEffect,
+  useState,
+  useRef,
+  useCallback,
+  useMemo,
+} from "react";
+import Stack from "@mui/material/Stack";
+import { WaveSurfer, WaveForm, Region } from "wavesurfer-react";
 
-type MuiAudioPlayerProps =
-  | (MuiHtmlAudioElementProps & {
-      useWebAudioApi: false;
-    })
-  | (WebAudioElementProps & {
-      useWebAudioApi: true;
-    });
+import {
+  Grid,
+  LinearProgress,
+  Box,
+  useTheme,
+  Palette,
+  SxProps,
+  Theme,
+  Paper,
+} from "@mui/material";
+import IconButton, { IconButtonProps } from "@mui/material/IconButton";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import Typography from "@mui/material/Typography";
+import PauseIcon from "@mui/icons-material/Pause";
+import {
+  WaveSurferProps,
+  WaveSurferRef,
+} from "wavesurfer-react/dist/containers/WaveSurfer";
+import Slider from "@mui/material/Slider";
 
+type MuiAudioPlayerProps = {
+  src: string;
+  size?: "small" | "medium" | "large";
+  paperize?: boolean;
+
+  id?: string;
+  waveColor?: keyof Palette | string;
+  waveHeight?: number;
+
+  playPauseIconButtonProps?: IconButtonProps;
+
+  display?: "waveform" | "timeline";
+  inline?: boolean;
+  showTimestamps?: boolean;
+
+  containerSx?: SxProps<Theme>;
+  containerHeight?: string | number;
+  containerWidth?: string | number;
+};
 const MuiAudioPlayer = ({
-  useWebAudioApi = false,
+  src,
+  size = "medium",
+  id = "audio-player",
+  display = "waveform",
+  showTimestamps = true,
 
+  containerWidth = 250,
+  containerHeight = "auto",
+  paperize = true,
+  waveHeight = 48,
+  inline = false,
+  playPauseIconButtonProps,
   ...props
 }: MuiAudioPlayerProps) => {
-  if (!useWebAudioApi) return <MuiHtmlAudioElement {...props} />;
-  return <WebAudioElement />;
+  const [loading, setLoading] = useState(true);
+
+  const [progress, setProgress] = useState(0);
+  const [playing, setPlaying] = useState(false);
+  const [position, setPosition] = useState(0);
+  const [currentTime, setCurrentTime] = useState("00:00");
+  const [endTime, setEndTime] = useState("");
+  const wavesurferRef = React.useRef<WaveSurferRef>();
+
+  const handleMount: WaveSurferProps[`onMount`] = React.useCallback(
+    (waveSurfer) => {
+      wavesurferRef.current = waveSurfer;
+      if (wavesurferRef.current) {
+        if (src) {
+          wavesurferRef.current.load(src);
+        }
+
+        wavesurferRef.current.on("ready", () => {
+          setLoading(false);
+          if (wavesurferRef?.current?.getDuration()) {
+            setEndTime(
+              secondsToTimestring(wavesurferRef?.current?.getDuration())
+            );
+          }
+        });
+
+        wavesurferRef.current.on("loading", (n: number) => {
+          setProgress(n);
+        });
+
+        [`finish`, `error`, `destroy`, `pause`].forEach((e) =>
+          wavesurferRef.current?.on(e, () => setPlaying(false))
+        );
+
+        wavesurferRef.current.on("error", (e) => console.error(e));
+
+        wavesurferRef.current.on("playing", () => setPlaying(true));
+
+        wavesurferRef.current.on("audioprocess", (e: number) => {
+          setCurrentTime(secondsToTimestring(e));
+        });
+      }
+    },
+    [src]
+  );
+
+  const audioElement = useMemo(() => {
+    if (display != "timeline") return null;
+    const audio = new Audio(src);
+    audio.addEventListener("playing", () => setPlaying(true));
+    ["pause", "ended"].forEach((e) =>
+      audio.addEventListener(e, () => setPlaying(false))
+    );
+    audio.addEventListener("canplaythrough", () => {
+      setLoading(false);
+      setEndTime(secondsToTimestring(audio.duration));
+    });
+    audio.addEventListener("timeupdate", () => {
+      setCurrentTime(secondsToTimestring(audio.currentTime));
+      setPosition((audio.currentTime / audio.duration) * 100);
+    });
+
+    audio.addEventListener("error", (e) => console.error(e));
+    return audio;
+  }, [src, display]);
+
+  const handlePlay = () => {
+    if (display == "timeline") {
+      console.log(playing);
+      if (!playing) {
+        console.info(`playing`);
+        audioElement?.play();
+      } else audioElement?.pause();
+      return;
+    }
+    if (!wavesurferRef.current) return;
+    if (playing) {
+      return wavesurferRef.current.pause();
+    }
+    wavesurferRef.current.play();
+
+    setPlaying(true);
+  };
+
+  const theme = useTheme();
+
+  const waveColor = props.waveColor || theme.palette.primary.main;
+  const PlayPauseButton = useCallback(
+    () =>
+      loading && !playing ? null : (
+        <IconButton
+          onClick={handlePlay}
+          color="primary"
+          {...playPauseIconButtonProps}
+        >
+          {playing ? (
+            <PauseIcon fontSize={size} />
+          ) : (
+            <PlayArrowIcon fontSize={size} />
+          )}
+        </IconButton>
+      ),
+    [size, playPauseIconButtonProps, playing, loading]
+  );
+
+  if (!src) return null;
+  return (
+    <Stack
+      sx={{
+        ...(props.containerSx || {}),
+        height: containerHeight,
+        width: containerWidth,
+      }}
+      direction={inline ? `row` : `column`}
+      component={paperize ? Paper : "div"}
+      alignItems="center"
+    >
+      {loading && (
+        <LinearProgress
+          variant="determinate"
+          style={{ flexGrow: 1 }}
+          value={progress}
+        />
+      )}
+      {inline && (
+        <Box p={1}>
+          <PlayPauseButton />
+        </Box>
+      )}
+
+      <Stack
+        component={Box}
+        direction="row"
+        flexGrow={1}
+        height={"100%"}
+        width="100%"
+        alignItems="center"
+        spacing={1}
+      >
+        {showTimestamps && (
+          <Box pl={1}>
+            <small
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                alignContent: "center",
+                height: "100%",
+              }}
+            >
+              {currentTime}
+            </small>
+          </Box>
+        )}
+        <Box flexGrow={1} height={"100%"} width="100%" alignItems="center">
+          {display == "waveform" && (
+            <WaveSurfer onMount={handleMount}>
+              <WaveForm
+                id={id}
+                fillParent
+                mediaControls
+                waveColor={waveColor}
+                height={waveHeight}
+                hideScrollbar={true}
+              />
+            </WaveSurfer>
+          )}
+          {display == "timeline" && !loading && (
+            <Box mx={1} display="flex" alignItems="center" height="100%">
+              <Slider
+                onChange={(e, v) => {
+                  if (audioElement && typeof v == "number")
+                    audioElement.fastSeek((audioElement.duration / 100) * v);
+                }}
+                size="small"
+                value={position}
+              />
+            </Box>
+          )}
+        </Box>
+
+        {showTimestamps && (
+          <Box pr={1}>
+            <small
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                alignContent: "center",
+                height: "100%",
+              }}
+            >
+              {endTime}
+            </small>
+          </Box>
+        )}
+      </Stack>
+      <Box display="flex" p={1} justifyContent="center" alignItems="center">
+        {!inline && (
+          <Stack spacing={1} direction="row">
+            <PlayPauseButton />
+          </Stack>
+        )}
+      </Box>
+    </Stack>
+  );
 };
 
-export default MuiAudioPlayer;
+const secondsToTimestring = (seconds: number) => {
+  const date = new Date();
+  date.setHours(0);
+  date.setMinutes(0);
+  date.setSeconds(seconds);
+  return date.toTimeString().slice(3, 8);
+};
+export default React.memo(MuiAudioPlayer);
